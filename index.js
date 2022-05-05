a1f6ea1f0dffd2045eb9a536b9ac6eeb78d2503d
Virgilia Antonucci <virgilia.antonucci@gmail.com>
2022-05-05 18:54:12 -0400

    commit tree bad93c82f39b99a27cb16ab375f5e05a8a290e03

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(array, string) {
+    return array.filter(word => word.toLowerCase() == string.toLowerCase())
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(word => word.startsWith(string))
+}
+
+function matchName(array, string) {
+    return array.filter(word => word['name'].toLowerCase() == string.toLowerCase())
+}
